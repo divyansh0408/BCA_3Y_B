6ac7e9e672e0e418315820a2fb895e36fbaa1c53
divyansh0408 <divyanshchaudhary912@gmail.com>
2024-08-12 11:38:54 +0530

    fourth commit

---
+++ first.js
@@ -0,0 +1,39 @@
+// let a = 10;
+// let b = 5;
+// let sum = a + b;
+// let diff = a - b;
+// let product = a * b;
+// let quotient = a / b;
+// let reminder = a % b;
+// let isequal = (a == b);
+// let npteqaul = (a != b);
+// let isgreater = (a > b);
+// let logicaland = (a > b && b < 10);
+// document.write(sum, "<br>", diff, "<br>", product, "<br>", quotient, "<br>", reminder, "<br>", isequal, "<br>", npteqaul, "<br>", isgreater, "<br>", logicaland);
+// -------------------------------------------------------
+// let fname = "Divyansh ";
+// document.write("Hello ", fname);
+// let age = 20;
+// document.write("<br>Age ", age, "<br>");
+// let isStudent = true;
+// document.write(`Student - ${isStudent ? "yes" : "no"}`, "<br>");
+// let emptyVlaue = null;
+// document.write(`About us  - ${emptyVlaue || "Write something"}`, "<br>");
+// let uninitialized;
+// document.write(!uninitialized && "Fill details completely");
+//-----------------------------------------------------------------
+
+// let age=10;
+// if(age>0){
+// document.write("You are eligible for vote");
+// }
+// else 
+// {
+//   document.write("You are not eligible for vote");
+  
+// }
+//              OR
+
+let age=18;
+document.write(age>17?"you are eligible to vote":"You are not eligible to vote");
+
